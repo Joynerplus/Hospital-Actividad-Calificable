f2ca7e7d587f25adbbbe224d55eff53907828667
Joynerplus <joynerplus@msn.com>
2023-05-11 20:26:32 -0500

    Update volver.js

--- JS/volver.js
+++ JS/volver.js
@@ -1,3 +1,3 @@
 document.getElementById("botonVolver").addEventListener("click", function () {
-    window.location.href = "joynerplus.github.io/Hospital-Actividad-Calificable/";
+    window.location.href = "../index.html";
 });
